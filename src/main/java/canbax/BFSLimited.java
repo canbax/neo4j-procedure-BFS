74a5da84daf45162d70a4267217e4d99218786a0
canbax <yusufsaidcanbaz@gmail.com>
2020-08-14 23:09:44 +0300

    bfs with queue

--- src/main/java/canbax/BFSLimited.java
+++ src/main/java/canbax/BFSLimited.java
@@ -20,16 +20,40 @@ public class BFSLimited {
     public Log log;
 
     /**
-     *
      * @param ids
-     * @param lengthLimit
-     * @param isDirected
+     * @param depthLimit
      * @return
      */
     @Procedure(value = "BFS", mode = Mode.WRITE)
     @Description("finds the minimal sub-graph from given nodes")
-    public Stream<Output> BFS(@Name("ids") List<Long> ids, @Name("lengthLimit") long lengthLimit, @Name("isDirected") boolean isDirected) {
+    public Stream<Output> BFS(@Name("ids") List<Long> ids, @Name("depthLimit") long depthLimit) {
         Output o = new Output();
+
+        // always return the source nodes
+        for (long id : ids) {
+            o.nodes.add(this.db.getNodeById(id));
+        }
+
+        Queue<Long> queue = new LinkedList<>(ids);
+        HashSet<Long> visitedNodes = new HashSet<>(ids);
+
+        while (!queue.isEmpty()) {
+            Node n1 = this.db.getNodeById(queue.remove());
+
+            Iterable<Relationship> edges = n1.getRelationships();
+            for (Relationship e : edges) {
+                Node n2 = e.getOtherNode(n1);
+                if (visitedNodes.contains(n2.getId())) {
+                    continue;
+                }
+                visitedNodes.add(n1.getId());
+                queue.add(n2.getId());
+                o.edges.add(e);
+                o.nodes.add(n2);
+            }
+        }
+
+
         return Stream.of(o);
     }
 
